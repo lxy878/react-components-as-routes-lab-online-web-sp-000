9c1638c1ebdf276412041a1397b68a6b9aaab618
Xiaoyi Liu <lxy878@gmail.com>
2020-11-11 12:38:53 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,8 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => <div key={director.name}>{director.name}<ul>{director.movies.map(movie=><li key={movie}>{movie}</li>)}</ul></div>)}
     </div>
   );
 }
