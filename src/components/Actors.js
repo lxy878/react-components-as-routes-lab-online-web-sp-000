46dacff362bc2ae7b04586933c86a53edf72002e
Xiaoyi Liu <lxy878@gmail.com>
2020-11-11 12:38:14 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,8 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor=><div key={actor.name}>{actor.name}<ul>{actor.movies.map(movie => <li key={movie}>{movie}</li>)}</ul></div>)}
     </div>
   );
 };
